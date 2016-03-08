2ab4306ee271f0406ce7722b53241cbea42e1d50
TimothyStiles <TimothySStiles@gmail.com>
2016-03-08 08:14:42 -0500

    changed project name.

--- test/cluster_difference_vis/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cluster-difference-vis.core-test
-  (:require [clojure.test :refer :all]
-            [cluster-difference-vis.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

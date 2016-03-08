8538b6fc642201bc4b7ee801498f3afd17074da8
TimothyStiles <TimothySStiles@gmail.com>
2016-03-08 08:13:58 -0500

    random functions can now be generated and evaluated to produce graphs.

--- src/bezier-generator/core.clj
+++ src/bezier-generator/core.clj
@@ -14,23 +14,53 @@
        (svg/serialize)
        (spit path)))
 
+(defn random-scalar ;scalar turned down for testing.
+  []
+  (let [bool (rand-nth [true false])]
+        (if bool 
+          (rand-nth (range 1 2))
+          (rand-nth (map #(/ % 2) (range 1 2))))))
+
+(random-scalar)
+(defmacro rand-op
+  []
+  '(rand-nth ['+ '*]))
+
+(defmacro rand-wave
+  []
+  '(list '* (random-scalar) (list (rand-nth ['Math/sin 'Math/cos]) (list '* (random-scalar) 'x))))
+
+(rand-wave)
+;switch apply to cons for better results
+(defmacro function-generator
+  []
+  '(list 'fn '[x] (cons (rand-op) (repeatedly (rand-nth (range 1 6)) #(rand-wave)))))
+
+(function-generator)
+(eval 5)
+(let [functions (map eval (repeatedly 2 #(function-generator)))]
+  (map #(%1 %2) functions [1 2]))
+(map eval (repeatedly 2 #(function-generator)))
+(repeatedly 1 #(function-generator))
 (def spec
   {:x-axis (viz/linear-axis
-            {:domain [-2 2]
+            {:domain [-1.8 1.8]
              :range  [50 590]
              :pos    -1})
    :y-axis (viz/linear-axis
-            {:domain      [-1 1]
+            {:domain      [-1.8 1.8]
              :range       [550 20]
              :pos         -1})
    :grid   {:attribs {:stroke "#caa"}
             :minor-x false
             :minor-y false}
-   :data   [{:values  (map (juxt #(- (Math/sin %) (Math/sin (* 2.3 %))) #(Math/cos %)) (range 0 200 0.01))
+   :data   [{:values  (map (juxt (eval (function-generator)) (eval (function-generator))) 
+                           (range 0 300 0.01))
              :attribs {:fill "#0af" :stroke "none"}
              :layout  viz/svg-scatter-plot}
             ]})
-
+(fn [x] (* (* 1 (Math/cos (* 1/2 x))) (* 1 (Math/sin (* 1 x))) (* 1 (Math/cos (* 1 x)))))
+(eval) (function-generator)
 (export-viz spec "bezier.svg")
 (-> spec
     (assoc :y-axis (viz/log-axis

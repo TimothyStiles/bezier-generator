e14882f5e0858272fac807bf9df9fea5bfb8c151
Tim <TimothyStiles@users.noreply.github.com>
2016-03-18 15:55:55 -0400

    changed license tag.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject golo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache Software License"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [thi.ng/geom "0.0.908"]])
